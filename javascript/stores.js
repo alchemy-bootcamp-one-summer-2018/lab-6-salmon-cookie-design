8b159687eaa0be7eba7ee48c6f4b617ad41bd828
Jack <ajackofsometrades@gmail.com>
2018-07-31 16:02:51 -0700

    working random hour function

--- javascript/stores.js
+++ javascript/stores.js
@@ -43,5 +43,28 @@ var stores = [
     },
 ];
 
+function addHRArray(storeArray) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    function getRandomHRs(min, max) {
+        max = Math.floor(max);
+        min = Math.ceil(min);
+        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    }
+
+    for(var i = 0; i < storeArray.length; i++){
+     
+        var hourArray = [];
+        for(var hr = 0; hr < 14; hr++){
+            hourArray.push(
+                getRandomHRs(storeArray[i]['minCust'], storeArray[i]['maxCust'])
+            );
+        }
+        storeArray[i]['perHour'] = hourArray;
+        
+    }
+}
+
+addHRArray(stores);
+
 
 
