f162db9c6da6be3a442ed90526d82b0eb8145e0b
Jack <ajackofsometrades@gmail.com>
2018-07-31 14:57:31 -0700

    stores as array

--- javascript/stores.js
+++ javascript/stores.js
@@ -1,16 +1,47 @@
+/* globals stores */
+
+var stores = [
+    {
+        name: 'Pike Place Market',
+        key: 'pike',
+        minCust: 23,
+        maxCust: 65,
+        perSale: 6.3,
+        perHour: null,
+    },
+    {
+        name: 'SeaTac Airport',
+        key: 'seatac',
+        minCust: 3,
+        maxCust: 24,
+        perSale: 1.2,
+        perHour: null,
+    }, 
+    {
+        name: 'Seattle Center',
+        key: 'seattlecenter',
+        minCust: 11,
+        maxCust: 38,
+        perSale: 3.7,
+        perHour: null,
+    },
+    {
+        name: 'Capitol Hill',
+        key: 'caphill',
+        minCust: 20,
+        maxCust: 38,
+        perSale: 2.3,
+        perHour: null,
+    },
+    {
+        name: 'Alki',
+        key: 'alki',
+        minCust: 2,
+        maxCust: 16,
+        perSale: 4.6,
+        perHour: null,
+    },
+];
 
 
-function Store(name, key, minCust, maxCust, perSale) {
-    this.name = name;
-    this.key = key;
-    this.minCust = minCust;
-    this.maxCust = maxCust;
-    this.perSale = perSale;
-    this.perHour = null;
-}
 
-var pikePlace = new Store('Pike Place Market', 'pike', 23, 65, 6.3);
-var seaTac = new Store('SeaTac Airport', 'seatac', 3, 24, 1.2);
-var seaCent = new Store('Seattle Center', 'seattlecenter', 11, 38, 3.7);
-var capHill = new Store('Capitol Hill', 'caphill', 20, 38, 2.3);
-var alki = new Store('Alki', 'alki', 2, 16, 4.6);
