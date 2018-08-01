c1a55be2ad3e117c53c6f14c372acbbe450ea28a
Jack <ajackofsometrades@gmail.com>
2018-08-01 13:49:53 -0700

    stores-new.js is now stores.js

--- javascript/stores.js
+++ javascript/stores.js
@@ -1,91 +1,117 @@
-/* exported stores */
+'use strict';
+
+(function(module) {
+
+     var stores = [
+        {
+            name: 'Pike Place Market',
+            key: 'pike',
+            custPerHourMin: 23,
+            custPerHourMax: 65,
+            cookiesPerCust: 6.3,
+        },
+        {
+            name: 'SeaTac Airport',
+            key: 'seatac',
+            custPerHourMin: 3,
+            custPerHourMax: 24,
+            cookiesPerCust: 1.2,
+        },
+        {
+            name: 'Seattle Center',
+            key: 'seattlecenter',
+            custPerHourMin: 11,
+            custPerHourMax: 38,
+            cookiesPerCust: 3.7,
+        },
+        {
+            name: 'Capitol Hill',
+            key: 'caphill',
+            custPerHourMin: 20,
+            custPerHourMax: 38,
+            cookiesPerCust: 2.3,
+        },
+        {
+            name: 'Alki',
+            key: 'alki',
+            custPerHourMin: 2,
+            custPerHourMax: 16,
+            cookiesPerCust: 4.6,
+        },
+    ];
+
+    var totals = {
+        name: 'Totals',
+        hours: [],
+    };
 
 
-var stores = [
-    {
-        name: 'Pike Place Market',
-        key: 'pike',
-        custPerHourMin: 23,
-        custPerHourMax: 65,
-        cookiesPerCust: 6.3,
-    },
-    {
-        name: 'SeaTac Airport',
-        key: 'seatac',
-        custPerHourMin: 3,
-        custPerHourMax: 24,
-        cookiesPerCust: 1.2,
-    }, 
-    {
-        name: 'Seattle Center',
-        key: 'seattlecenter',
-        custPerHourMin: 11,
-        custPerHourMax: 38,
-        cookiesPerCust: 3.7,
-    },
-    {
-        name: 'Capitol Hill',
-        key: 'caphill',
-        custPerHourMin: 20,
-        custPerHourMax: 38,
-        cookiesPerCust: 2.3,
-    },
-    {
-        name: 'Alki',
-        key: 'alki',
-        custPerHourMin: 2,
-        custPerHourMax: 16,
-        cookiesPerCust: 4.6,
-    },
-];
+    function custByHour(storeArray) {
 
-function custByHour(storeArray) {
-
-    function getRandomHRs(min, max) {
-        max = Math.floor(max);
-        min = Math.ceil(min);
-        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
-    }
-
-    for(var i = 0; i < storeArray.length; i++){
-     
-        var hourArray = [];
-        for(var hr = 0; hr < 14; hr++){
-            hourArray.push(getRandomHRs(storeArray[i]['custPerHourMin'], storeArray[i]['custPerHourMax']));
+        function getRandomHRs(min, max) {
+            max = Math.floor(max);
+            min = Math.ceil(min);
+            return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
         }
-        storeArray[i]['custByHour'] = hourArray;
 
-    }
-}
+        for (var i = 0; i < storeArray.length; i++) {
 
-function cookiesByHour(storeArray) {
-    for(var i = 0; i < storeArray.length; i++){
-        storeArray[i]['cookiesByHour'] = [];
-        for(var hr = 0; hr < 14; hr++) {
-            var hrSales = storeArray[i]['custByHour'][hr] * storeArray[i]['cookiesPerCust'];
-            hrSales = hrSales.toPrecision(2);
-            hrSales = parseFloat(hrSales);
-            storeArray[i]['cookiesByHour'][hr] = hrSales;
+            var hourArray = [];
+            for (var hr = 0; hr < 14; hr++) {
+                hourArray.push(getRandomHRs(storeArray[i]['custPerHourMin'], storeArray[i]['custPerHourMax']));
+            }
+            storeArray[i]['custByHour'] = hourArray;
+
         }
     }
-}
 
-function cookiesPerDay(storeArray) {
+    function cookiesByHour(storeArray) {
+        for (var i = 0; i < storeArray.length; i++) {
+            storeArray[i]['cookiesByHour'] = [];
+            for (var hr = 0; hr < 14; hr++) {
+                var hrSales = storeArray[i]['custByHour'][hr] * storeArray[i]['cookiesPerCust'];
+                hrSales = hrSales.toPrecision(2);
+                hrSales = parseFloat(hrSales);
+                storeArray[i]['cookiesByHour'][hr] = hrSales;
+            }
+        }
+    }
 
-    for(var i = 0; i < storeArray.length; i++) {
+    function cookiesPerDay(storeArray) {
+
+        for (var i = 0; i < storeArray.length; i++) {
+            var rowSum = 0;
+            for (var hr = 0; hr < 14; hr++) {
+                rowSum += storeArray[i]['cookiesByHour'][hr];
+            }
+            storeArray[i]['cookiesPerDay'] = rowSum;
+        }
+    }
+
+    function cookiesByHourTotals(storeArray, totalsObj, key) {
+        for (var i = 0; i < 13; i++) {
+            var columnTotal = 0;
+            for (var j = 0; j < storeArray.length; j++) {
+                columnTotal += storeArray[j]['cookiesByHour'][i];
+            }
+            totalsObj[key][i] = columnTotal;
+        }
+
+        // this should be a separate function called cookiesPerDayTotals
         var rowSum = 0;
-        for(var hr = 0; hr < 14; hr++) {
-            rowSum += storeArray[i]['cookiesByHour'][hr];
+
+        for (var hr = 0; hr < 13; hr++) {
+            rowSum += totalsObj[key][hr];
         }
-        storeArray[i]['cookiesPerDay'] = rowSum;
+        totalsObj[key].push(rowSum);
+
     }
-}
 
 
-custByHour(stores);
-cookiesByHour(stores);
-cookiesPerDay(stores);
+    module.stores = stores;
+    module.custByHour = custByHour;
+    module.cookiesByHour = cookiesByHour;
+    module.cookiesPerDay = cookiesPerDay;
+    module.cookiesByHourTotals = cookiesByHourTotals;
 
-
-
-
+})(window.module = window.module || {});
