3dd0e13666e1af090a758a435b33adff008da560
Jack <ajackofsometrades@gmail.com>
2018-08-01 13:49:00 -0700

    all data modification in stores.js now

--- javascript/stores-new.js
+++ javascript/stores-new.js
@@ -40,6 +40,12 @@
         },
     ];
 
+    var totals = {
+        name: 'Totals',
+        hours: [],
+    };
+
+
     function custByHour(storeArray) {
 
         function getRandomHRs(min, max) {
@@ -82,9 +88,30 @@
         }
     }
 
+    function cookiesByHourTotals(storeArray, totalsObj, key) {
+        for (var i = 0; i < 13; i++) {
+            var columnTotal = 0;
+            for (var j = 0; j < storeArray.length; j++) {
+                columnTotal += storeArray[j]['cookiesByHour'][i];
+            }
+            totalsObj[key][i] = columnTotal;
+        }
+
+        // this should be a separate function called cookiesPerDayTotals
+        var rowSum = 0;
+
+        for (var hr = 0; hr < 13; hr++) {
+            rowSum += totalsObj[key][hr];
+        }
+        totalsObj[key].push(rowSum);
+
+    }
+
+
     module.stores = stores;
     module.custByHour = custByHour;
     module.cookiesByHour = cookiesByHour;
     module.cookiesPerDay = cookiesPerDay;
+    module.cookiesByHourTotals = cookiesByHourTotals;
 
 })(window.module = window.module || {});
