07b94b7b0151043986edc22539efa353973a110f
Jack <ajackofsometrades@gmail.com>
2018-08-01 03:22:19 -0700

    stores module done (split into more modules?)

--- javascript/stores-new.js
+++ javascript/stores-new.js
@@ -40,10 +40,51 @@
         },
     ];
 
-    // for(var i = 0; i < fruits.length; i++) {
-    //     fruits[i].someProperty = 'prop';
-    // }
+    function custByHour(storeArray) {
+
+        function getRandomHRs(min, max) {
+            max = Math.floor(max);
+            min = Math.ceil(min);
+            return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
+        }
+
+        for (var i = 0; i < storeArray.length; i++) {
+
+            var hourArray = [];
+            for (var hr = 0; hr < 14; hr++) {
+                hourArray.push(getRandomHRs(storeArray[i]['custPerHourMin'], storeArray[i]['custPerHourMax']));
+            }
+            storeArray[i]['custByHour'] = hourArray;
+
+        }
+    }
+
+    function cookiesByHour(storeArray) {
+        for (var i = 0; i < storeArray.length; i++) {
+            storeArray[i]['cookiesByHour'] = [];
+            for (var hr = 0; hr < 14; hr++) {
+                var hrSales = storeArray[i]['custByHour'][hr] * storeArray[i]['cookiesPerCust'];
+                hrSales = hrSales.toPrecision(2);
+                hrSales = parseFloat(hrSales);
+                storeArray[i]['cookiesByHour'][hr] = hrSales;
+            }
+        }
+    }
+
+    function cookiesPerDay(storeArray) {
+
+        for (var i = 0; i < storeArray.length; i++) {
+            var rowSum = 0;
+            for (var hr = 0; hr < 14; hr++) {
+                rowSum += storeArray[i]['cookiesByHour'][hr];
+            }
+            storeArray[i]['cookiesPerDay'] = rowSum;
+        }
+    }
 
     module.stores = stores;
+    module.custByHour = custByHour;
+    module.cookiesByHour = cookiesByHour;
+    module.cookiesPerDay = cookiesPerDay;
 
 })(window.module = window.module || {});
