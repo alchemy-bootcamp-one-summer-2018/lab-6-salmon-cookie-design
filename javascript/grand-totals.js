418e3a251c037951a1af7991a07f5f549fe2cec8
Jack <ajackofsometrades@gmail.com>
2018-08-01 16:24:49 -0700

    moved totals into two files... must export vars

--- javascript/grand-totals.js
+++ javascript/grand-totals.js
@@ -2,3 +2,42 @@
 //     Calculating totals from stores data
 //     A totals object exported from stores.js
 
+'use strict';
+
+(function(module) {
+
+
+    let stats = [
+        {
+            name: 'Hourly Totals for All Locations',
+            cookiesByHour: [],
+            cookiesPerDay: [],
+
+        },
+    ];
+
+    function cookiesByHourTotals(storeArray, statsArray, statIndex, stat1, stat2) {
+        for(let i = 0; i < 13; i++) {
+            let columnTotal = 0;
+            for(let j = 0; j < storeArray.length; j++) {
+                columnTotal += storeArray[j]['cookiesByHour'][i];
+            }
+            statsArray[statIndex][stat1][i] = columnTotal;
+        }
+
+        // this should be a separate function called cookiesPerDayTotals or cookiesByHourTotalsTotal
+        let rowSum = 0;
+
+        for(let hr = 0; hr < 13; hr++) {
+            rowSum += statsArray[statIndex][stat1][hr];
+        }
+        statsArray[statIndex][stat2].push(rowSum);
+
+    }
+
+
+    cookiesByHourTotals(stores, stats, 0, 'cookiesByHour', 'cookiesPerDay');
+    module.cookiesByHourTotals = cookiesByHourTotals;
+
+})(window.module = window.module || {});
+
--- javascript/store-table.js
+++ javascript/store-table.js
@@ -1,1 +1,95 @@
-// populates the tbody based on data in stores
+'use strict';
+
+(function(module) {
+
+    let stores = [
+        {
+            name: 'Pike Place Market',
+            key: 'pike',
+            custPerHourMin: 23,
+            custPerHourMax: 65,
+            cookiesPerCust: 6.3,
+        },
+        {
+            name: 'SeaTac Airport',
+            key: 'seatac',
+            custPerHourMin: 3,
+            custPerHourMax: 24,
+            cookiesPerCust: 1.2,
+        },
+        {
+            name: 'Seattle Center',
+            key: 'seattlecenter',
+            custPerHourMin: 11,
+            custPerHourMax: 38,
+            cookiesPerCust: 3.7,
+        },
+        {
+            name: 'Capitol Hill',
+            key: 'caphill',
+            custPerHourMin: 20,
+            custPerHourMax: 38,
+            cookiesPerCust: 2.3,
+        },
+        {
+            name: 'Alki',
+            key: 'alki',
+            custPerHourMin: 2,
+            custPerHourMax: 16,
+            cookiesPerCust: 4.6,
+        },
+    ];
+
+    function custByHour(storeArray) {
+
+        function getRandomHRs(min, max) {
+            max = Math.floor(max);
+            min = Math.ceil(min);
+            return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is exclusive and the minimum is inclusive
+        }
+
+        for(let i = 0; i < storeArray.length; i++) {
+
+            let hourArray = [];
+            for(let hr = 0; hr < 13; hr++) {
+                hourArray.push(getRandomHRs(storeArray[i]['custPerHourMin'], storeArray[i]['custPerHourMax']));
+            }
+            storeArray[i]['custByHour'] = hourArray;
+
+        }
+    }
+
+    function cookiesByHour(storeArray) {
+        for(let i = 0; i < storeArray.length; i++) {
+            storeArray[i]['cookiesByHour'] = [];
+            for(let hr = 0; hr < 13; hr++) {
+                let hrSales = storeArray[i]['custByHour'][hr] * storeArray[i]['cookiesPerCust'];
+                hrSales = hrSales.toPrecision(2);
+                hrSales = parseFloat(hrSales);
+                storeArray[i]['cookiesByHour'][hr] = hrSales;
+            }
+        }
+    }
+
+    function cookiesPerDay(storeArray) {
+
+        for(let i = 0; i < storeArray.length; i++) {
+            let rowSum = 0;
+            for(let hr = 0; hr < 13; hr++) {
+                rowSum += storeArray[i]['cookiesByHour'][hr];
+            }
+            storeArray[i]['cookiesPerDay'] = rowSum;
+        }
+    }
+
+    custByHour(stores);
+    cookiesByHour(stores);
+    cookiesPerDay(stores);
+    
+    module.stores = stores;
+    module.stats = stats;
+    module.custByHour = custByHour;
+    module.cookiesByHour = cookiesByHour;
+    module.cookiesPerDay = cookiesPerDay;
+
+})(window.module = window.module || {});
