2e59f5504df3c95462d5a53df264f1e258ba02c0
Jack <ajackofsometrades@gmail.com>
2018-08-01 16:43:30 -0700

    new module: initStoreForm

---
+++ javascript/store-form.js
@@ -0,0 +1,14 @@
+
+
+'use strict';
+
+(function (module) {
+
+    function initStoreForm(onStoreAdded) {
+        
+        
+    }
+
+    module.initStoreForm(onStoreAdded) = initStoreForm(onStoreAdded);
+
+})(window.module = window.module || {});
