ecd92ab7e93e0f02ad33cc096bed9c8a7cde99dc
Jack <ajackofsometrades@gmail.com>
2018-08-01 03:49:57 -0700

    I'm getting it, but I need to sleep

--- javascript/index-new.js
+++ javascript/index-new.js
@@ -2,25 +2,40 @@
 
 (function(module){
     var stores = module.stores;
+    var totals = module.stores;
     var toDOM = module.toDOM;
     var html = module.html;
 
     // reference the ul
-    var ul = document.getElementById('fruits');
+    var table = document.getElementById('module-table');
 
     // create a function we can call with data,
     // that returns DOM we can append into the 
     // document
-    var render = function(fruit) {
+    var render = function(stores, totals) {
         return toDOM(html`        
-            <li>
-                <h2>${fruit.name}</h2>
-                <img src="${fruit.image}" alt="${fruit.name}">
-                <span class="color-name" 
-                    style="background: ${fruit.color}">
-                    ${fruit.color}
-                </span>
-            </li>
+            <tbody>
+                <tr>
+                    <td>${stores.name}</td>
+                    <td>main cells</td> x 13
+                    <td class="end-totals" 
+                        style="background: navy">
+                        ${stores[i].cookiesPerDay}
+                    </td>    
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td>Hourly Totals for All Locations</td>
+                    <td style="background: navy">
+                        total cells
+                    </td> x 13
+                    <td class="end-totals" 
+                    style="background: navy">
+                        ${totals.[14]}
+                    </td>    
+                </tr>
+            </tfoot>
         `);
     };
 
@@ -32,7 +47,7 @@
         var dom = render(fruit);
 
         // append it to the ul
-        ul.appendChild(dom);
+        table.appendChild(dom);
     }
 
 })(window.module = window.module || {});
