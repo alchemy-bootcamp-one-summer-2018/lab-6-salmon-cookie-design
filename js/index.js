83cb6a400056f84db02b9f90184d342bf3d0e017
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-08-01 09:28:08 -0700

    cleaned up confusion on var names

--- js/index.js
+++ js/index.js
@@ -10,24 +10,24 @@
     // create a function we can call with data,
     // that returns DOM we can append into the 
     // document
-    var render = function(stores) {
+    var render = function(store) {
         return toDOM(html`
         <tr>
-            <td>${stores.name}</td>
-            <td>${stores.cookiesSalesPerStore[0]}</td>
-            <td>${stores.cookiesSalesPerStore[1]}</td>
-            <td>${stores.cookiesSalesPerStore[2]}</td>
-            <td>${stores.cookiesSalesPerStore[3]}</td>
-            <td>${stores.cookiesSalesPerStore[4]}</td>
-            <td>${stores.cookiesSalesPerStore[5]}</td>
-            <td>${stores.cookiesSalesPerStore[6]}</td>
-            <td>${stores.cookiesSalesPerStore[7]}</td>
-            <td>${stores.cookiesSalesPerStore[8]}</td>
-            <td>${stores.cookiesSalesPerStore[9]}</td>
-            <td>${stores.cookiesSalesPerStore[10]}</td>
-            <td>${stores.cookiesSalesPerStore[11]}</td>
-            <td>${stores.cookiesSalesPerStore[12]}</td>
-            <td>${stores.cookiesSalesPerStore[13]}</td>
+            <td>${store.name}</td>
+            <td>${store.cookiesSalesPerHour[0]}</td>
+            <td>${store.cookiesSalesPerHour[1]}</td>
+            <td>${store.cookiesSalesPerHour[2]}</td>
+            <td>${store.cookiesSalesPerHour[3]}</td>
+            <td>${store.cookiesSalesPerHour[4]}</td>
+            <td>${store.cookiesSalesPerHour[5]}</td>
+            <td>${store.cookiesSalesPerHour[6]}</td>
+            <td>${store.cookiesSalesPerHour[7]}</td>
+            <td>${store.cookiesSalesPerHour[8]}</td>
+            <td>${store.cookiesSalesPerHour[9]}</td>
+            <td>${store.cookiesSalesPerHour[10]}</td>
+            <td>${store.cookiesSalesPerHour[11]}</td>
+            <td>${store.cookiesSalesPerHour[12]}</td>
+            <td>${store.cookiesSalesPerHour[13]}</td>
         </tr>`);
     };
     
