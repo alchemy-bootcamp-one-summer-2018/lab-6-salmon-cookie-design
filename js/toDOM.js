240716f41e3204c64c7d35ca57251d5ed23c9283
Sophie Graham <sophiergraham1@gmail.com>
2018-08-01 17:28:28 -0700

    daily total

--- js/toDOM.js
+++ js/toDOM.js
@@ -1,11 +1,14 @@
-(function(module)) {
+'use strict';
 
-    funtion toDOM(html) {
+(function(module) {
+
+    function toDOM(html) {
         var template = document.createElement ('template');
         template.innerHTML = html;
         return template.content;
     }
 
     module.toDOM = toDOM;
-    module.html = string.raw;
-}
+    module.html = String.raw;
+
+})(window.module = window.module || {});
