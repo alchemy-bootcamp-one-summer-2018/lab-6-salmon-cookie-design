572f9b572c98fbf13a77645bcf0fcd418a53ec8d
Claire Flanagan <claireflanagan@gmail.com>
2018-08-01 17:26:09 -0700

    up to step 3

--- js/index.js
+++ js/index.js
@@ -1,45 +1,12 @@
 'use strict';
 
 (function(module){
-    var stores = module.stores;
-    var toDOM = module.toDOM;
-    var html = module.html;
-    var totals = module.totals;
+    let initStoreForm = module.initStoreForm;
+    let addStore = module.addStore;
 
-    var table = document.getElementById('stores');
-    var tfooter = document.getElementById('tfoot');
-
-    let render = function(store) {
-        return toDOM(html`
-            <tr>
-                <td>${store.name}</td>
-                <td>${store.cookieSalesPerStore[0]}</td> 
-                <td>${store.cookieSalesPerStore[1]}</td>
-                <td>${store.cookieSalesPerStore[2]}</td>
-                <td>${store.cookieSalesPerStore[3]}</td> 
-                <td>${store.cookieSalesPerStore[4]}</td>
-                <td>${store.cookieSalesPerStore[5]}</td>
-                <td>${store.cookieSalesPerStore[6]}</td> 
-                <td>${store.cookieSalesPerStore[7]}</td>
-                <td>${store.cookieSalesPerStore[8]}</td>
-                <td>${store.cookieSalesPerStore[9]}</td> 
-                <td>${store.cookieSalesPerStore[10]}</td>
-                <td>${store.cookieSalesPerStore[11]}</td>
-                <td>${store.cookieSalesPerStore[12]}</td> 
-                <td>${store.cookieSalesPerStore[13]}</td>
-                <td class="totals">${store.dailyStoreTotal}</td>
-            </tr>
-        `);
-    }; 
-
-    var store;
-    for(var i = 0; i < stores.length; i++) {
-        store = stores[i];
-        let dom = render(store);
-        table.appendChild(dom);
-    }
-
-    var totalsRow = render(totals);
-    tfooter.appendChild(totalsRow);
+    initStoreForm(function onStoreAdded(store) {
+        addStore(store);
+        console.log('store', store);
+    });
 
 })(window.module = window.module || {});
--- js/stores.js
+++ js/stores.js
@@ -2,7 +2,7 @@
 
 (function(module) {
     
-    var stores = [{
+    let stores = [{
         name: 'Pike Place Market',
         key: 'pike',
         minCust: 23,
@@ -34,35 +34,35 @@
         avgCookies: 5
     }];
 
-    var randomHoursArray = [];
-    var cookieSalesArray = [];
+    let randomHoursArray = [];
+    let cookieSalesArray = [];
 
-    for(var j = 0; j < stores.length; j++) {
+    for(let j = 0; j < stores.length; j++) {
 
         stores[j].cookieSalesPerStore = [];
 
-        for(var i = 0; i < 14; i++) {
-            var custPerHours = Math.floor(Math.random() * (stores[j].maxCust - stores[j].minCust)) + stores[j].minCust;
-            var cookieSales = custPerHours * stores[j].avgCookies;
-            console.log('cookie sales:', cookieSales); 
+        for(let i = 0; i < 14; i++) {
+            let custPerHours = Math.floor(Math.random() * (stores[j].maxCust - stores[j].minCust)) + stores[j].minCust;
+            let cookieSales = custPerHours * stores[j].avgCookies;
+            //console.log('cookie sales:', cookieSales); 
             
             randomHoursArray.push(custPerHours);
             cookieSalesArray.push(cookieSales);
             stores[j].cookieSalesPerStore.push(cookieSales);
         }
     
-        console.log('random cust per hour array', randomHoursArray);
-        console.log('cookie sales per hour array', cookieSalesArray);
+        //console.log('random cust per hour array', randomHoursArray);
+        //console.log('cookie sales per hour array', cookieSalesArray);
 
     }
 
-    console.log('stores array:::', stores[0].cookieSalesPerStore);
+    //console.log('stores array:::', stores[0].cookieSalesPerStore);
     console.log('stores array:', stores);
     module.stores = stores;
 
-    var totals = [];
+    let totals = [];
     for(let i = 0; i < 14; i++) {
-        var total = 0;
+        let total = 0;
 
         for(let j = 0; j < stores.length; j++) {
             total = total + stores[j].cookieSalesPerStore[i];
@@ -71,11 +71,11 @@
         totals.push(total);
     }
 
-    var grandTotal = totally(totals);
+    let grandTotal = totally(totals);
 
     function totally(totals) {
-        var grandTotal = 0;
-        for(var j = 0; j < 14; j++) {
+        let grandTotal = 0;
+        for(let j = 0; j < 14; j++) {
             grandTotal = grandTotal + totals[j];
         } 
         return grandTotal;
@@ -86,33 +86,33 @@
         cookieSalesPerStore: totals,
         dailyStoreTotal: grandTotal
     };
-    console.log('module.totals:', module.totals);
+    //console.log('module.totals:', module.totals);
     
-    var storeTotals = horizontalTotals(stores);
+    let storeTotals = horizontalTotals(stores);
     function horizontalTotals(stores) {
-        var storeTotals = 0;
+        let storeTotals = 0;
         
-        for(var j = 0; j < stores.length; j++) {
+        for(let j = 0; j < stores.length; j++) {
             stores[j].dailyStoreTotal = 0;
             
-            var salesPerHourPerStoreArray = stores[j].cookieSalesPerStore;
-            console.log('horizontal array aka salesPerHourPerStoreArray:', salesPerHourPerStoreArray);
+            let salesPerHourPerStoreArray = stores[j].cookieSalesPerStore;
+            //console.log('horizontal array aka salesPerHourPerStoreArray:', salesPerHourPerStoreArray);
             
-            for(var i = 0; i < 14; i++) {
+            for(let i = 0; i < 14; i++) {
                 stores[j].dailyStoreTotal += salesPerHourPerStoreArray[i];
             }
 
             storeTotals = storeTotals + stores[j].cookieSalesPerStore[j];
-            console.log('dailyStoreTotal', stores[j].dailyStoreTotal);
+            //console.log('dailyStoreTotal', stores[j].dailyStoreTotal);
             
         } 
         
     }
 
-    console.log('these are store totals', storeTotals);
+    //console.log('these are store totals', storeTotals);
 
 
-    console.log(module.totals);
+    //console.log(module.totals);
 
 
 })(window.module = window.module || {});
@@ -138,28 +138,28 @@
 
 // EXAMPLE:
 
-// for(var i = 0; i < stores.length; i++) {
+// for(let i = 0; i < stores.length; i++) {
 //     stores[i].storeTotals = [];
-//     var StoreTotal = totally(stores[i].cookieSalesPerStore);    
+//     let StoreTotal = totally(stores[i].cookieSalesPerStore);    
 //     stores[i].storeTotals.push(store totals);
 // }
 
 // module.stores = stores;
 
-// var totals = [];
-// for (var i = 0;  i < 14; i++) {
-//     var total = 0
-//     for(var j; j < stores.length; j++) {
+// let totals = [];
+// for (let i = 0;  i < 14; i++) {
+//     let total = 0
+//     for(let j; j < stores.length; j++) {
 //         total += stores[j].cookieSalesPerStore[i];
 // }
 // totals.push(total);
 // }
 
-// var grandTotal = totally(totals);
+// let grandTotal = totally(totals);
 
 // function totally (totals) {
-//     var grandTotal = 0;
-//     for(var j = 0; j < 14; j++) {
+//     let grandTotal = 0;
+//     for(let j = 0; j < 14; j++) {
 //         grandTotal += totals[j];
 //     }
 //     return grandTotal;
