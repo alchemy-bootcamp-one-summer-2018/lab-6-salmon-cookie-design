5dccb17b528381426efea2688844b5b2fe9cf678
warrioryoko <miangpellegri@gmail.com>
2018-08-03 13:00:25 -0700

    Added .eslintrc and .travis.yaml; fixed linting issues

--- stores.js
+++ stores.js
@@ -2,47 +2,47 @@
 
 (function(module) {
 
-    function createSalesData (min, max, avg) {
+    function createSalesData(min, max, avg) {
         //Takes the max and min number of customers per time and calculates a random
         //# in that range - then multiplies by the average number of items bought
         //feed in the data for each store into this function to generate random
         //hourly sales data points
-    
+
         return Math.floor(
             (
                 ((Math.random() * (max - min + 1)) + min) * avg
             )
-        );    
+        );
     }
-    
-    function store(name, key, min, max, avg, len) {
+
+    function Store(name, key, min, max, avg, len) {
         this.name = name;
         this.key = key;
         this.min = min;
         this.max = max;
         this.avg = avg;
-    
+
         var hours = [];
         var finalHours = 0;
-        for (var h = 0; h < len; h++) {
+        for(var h = 0; h < len; h++) {
             hours.push(createSalesData(min, max, avg));
         }
 
-        for (var a = 0; a < len; a++) {
+        for(var a = 0; a < len; a++) {
             finalHours += hours[a];
         }
-    
+
         this.hours = hours;
         this.finalHours = finalHours;
-    };
-    
-    var pikeStore = new store('Pike Place Market', 'pike', 23, 65, 6.3, 14);
-    var seatacStore = new store('SeaTac Airport', 'seatac', 3, 24, 1.2, 14);
-    var seattleStore = new store('Seattle Center', 'seattlecenter', 11, 38, 3.7, 14);
-    var capitolStore = new store('Capitol Hill', 'caphill', 20, 38, 2.3, 14);
-    var alkiStore = new store('Alki', 'alki', 2, 16, 4.6, 14);
-    
-    var stores = [pikeStore, seatacStore, seattleStore, capitolStore, alkiStore];
+    }
+
+    var PikeStore = new Store('Pike Place Market', 'pike', 23, 65, 6.3, 14);
+    var SeatacStore = new Store('SeaTac Airport', 'seatac', 3, 24, 1.2, 14);
+    var SeattleStore = new Store('Seattle Center', 'seattlecenter', 11, 38, 3.7, 14);
+    var CapitolStore = new Store('Capitol Hill', 'caphill', 20, 38, 2.3, 14);
+    var AlkiStore = new Store('Alki', 'alki', 2, 16, 4.6, 14);
+
+    var stores = [PikeStore, SeatacStore, SeattleStore, CapitolStore, AlkiStore];
     var totals = [];
     var finalTotal = 0;
 
@@ -53,7 +53,7 @@
         }
         totals.push(total);
     }
-    for(let i = 0; i < totals.length; i++) {   
+    for(let i = 0; i < totals.length; i++) {
         finalTotal += totals[i];
     }
     totals.push(finalTotal);
